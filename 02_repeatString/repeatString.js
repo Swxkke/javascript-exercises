0a93b6c5109572027e7d5aeb31ac765c5a03698f
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-01-30 13:24:01 +0100

    helloworld and repeatstring ex done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const string = ""
+const num = ""
 
-};
+const repeatString = function(string, num) {
+    let repeater = string
+    if (num === 0) {
+        repeater = ""
+    } else if (num < 0) {
+        repeater = "ERROR"
+    } else {
+        for (i = 1; i < num; i++) {
+            repeater += `${string}`
+        }
+    }
+    return repeater;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
