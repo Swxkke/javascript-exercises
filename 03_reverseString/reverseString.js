392b14765121c9025719586e3f83037e2411e998
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-06 14:44:19 +0100

    niks gedaan kleine checkpoint

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const string = "Hello"
 
+const reverseString = function() {
+    for (let i = (string.length() - 1); i == 0 ; i--) {
+        let singleChar = string.charAt(i);
+        
+    }
 };
 
 // Do not edit below this line
