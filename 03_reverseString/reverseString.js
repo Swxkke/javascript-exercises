0c7b0e4bf99ca88cc9c5fc30f95669fb9ab88f20
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-07 14:53:08 +0100

    all tests done -> works in browser, not in tester

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
-const string = ""
+string = 'hello'
 
 const reverseString = function(string) {
-    const stringLength = (string.length - 1)
+    let stringLength = (string.length - 1)
     let newString = "";
     for (let i = stringLength; i >= 0; i--) {
         lastChar = string.substr(i, 1)
