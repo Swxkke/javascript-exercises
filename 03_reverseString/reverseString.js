806509f183d40949583cb846b6051b9f5071691c
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-07 10:51:36 +0100

    backup because VS is not working well

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,19 @@
-const string = "Hello"
 
-const reverseString = function() {
-    for (let i = (string.length() - 1); i == 0 ; i--) {
-        let singleChar = string.charAt(i);
-        
-    }
+const string = "Hello";
+
+const reverseString = function(string) {
+    stringLength = string.length;
+    console.log(stringLength);
+
+    // for (let i = (string.length() - 1); i == 0 ; i--) {
+    ////    let singleChar = string.slice(i);
+   //     newString = singleChar.concat();
+    //}
+    // return newString;
 };
+
+
+console.log(reverseString())
 
 // Do not edit below this line
 module.exports = reverseString;
