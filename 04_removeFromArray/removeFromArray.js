aa7e38bb0a6c8470b95c99e1b602860f2f0cecce
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-16 15:38:41 +0100

    1 element in remover works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,16 @@
-const removeFromArray = function(mainArr = [1, 2, 3, 4], remover = [3, 2]) {
-    let arrayLength = (mainArr.length - 1)
+const removeFromArray = function(mainArr = [1, 2, 3, 4], remover = [2,3]) {
+    let arrayLength = (mainArr.length)
     let removerLength = (remover.length -1)
+
 
     for (i = 0; i < arrayLength; i++) {
         let arrObj = mainArr[i];
-        for (y = 0; y < removerLength; i++) {
-            if (arrObj === remover) {
-                mainArr.splice(i, 1);
-            } else {
-                continue;
-            }
+        if (arrObj === remover) {
+            mainArr.splice(i, 1);
+            console.log(i)
+        } else {
+            console.log(i)
+            continue;
         }
     }
     return mainArr;
@@ -18,4 +19,4 @@ const removeFromArray = function(mainArr = [1, 2, 3, 4], remover = [3, 2]) {
 console.log(removeFromArray())
 
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
