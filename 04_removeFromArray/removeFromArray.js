fa994dad39a768bc3a37ce0950b20f296668e93c
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-16 14:56:14 +0100

    double loop - not tested live page bugging out

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,15 @@
-const removeFromArray = function(mainArr = [1, 2, 3, 4], remover = (3, 2)) {
+const removeFromArray = function(mainArr = [1, 2, 3, 4], remover = [3, 2]) {
     let arrayLength = (mainArr.length - 1)
+    let removerLength = (remover.length -1)
 
     for (i = 0; i < arrayLength; i++) {
         let arrObj = mainArr[i];
-        if (arrObj === remover) {
-            mainArr.splice(i, 1);
-            continue;
-        } else {
-
+        for (y = 0; y < removerLength; i++) {
+            if (arrObj === remover) {
+                mainArr.splice(i, 1);
+            } else {
+                continue;
+            }
         }
     }
     return mainArr;
