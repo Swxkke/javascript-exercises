349eb66f844f1d2b2457a9f0073c4e6a40c68128
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-16 16:30:58 +0100

    ex5 test 1 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(par1 = 1, par2 = 4) {
+    let sum = 0;
+    for (let i = par1; i <= par2; i++) {
+        sum += i
+        console.log(i)
+    }
+    return sum;
 };
+
+console.log(sumAll())
 
 // Do not edit below this line
 module.exports = sumAll;
