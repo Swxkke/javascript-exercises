f68f07ddacd5b5ebf42b7e81030aebada59bcef5
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-22 16:11:10 +0100

    working loops -> yet to test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,16 @@
-const sumAll = function(par1 = 1, par2 = 4) {
+const sumAll = function(par1 = 5,4, par2 = 1) {
     let sum = 0;
-    for (let i = par1; i <= par2; i++) {
-        sum += i
-        console.log(i)
+    if (par1 < par2) {
+        for (let i = par1; i <= par2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = par2; i <= par1; i++) {
+            sum += i;
+        }
+    }
+    if (sum === NaN || sum === undefined || sum <= 0) {
+        sum = "ERROR";
     }
     return sum;
 };
