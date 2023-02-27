8f8185e5d7feb0b72e203df6efc7ff8f0f80fcfe
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-02-27 14:36:14 +0100

    ex7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesC) {
+  let convertC = ((degreesC - 32) * (5/9));
+  let roundedC = convertC.toFixed(1);
+  let numberC = Number(roundedC)
+
+  return numberC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesF) {
+  let convertF = (degreesF * (9/5) + 32);
+  let roundedF = convertF.toFixed(1);
+  let numberF = Number(roundedF)
+
+  return numberF;
 };
 
 // Do not edit below this line
