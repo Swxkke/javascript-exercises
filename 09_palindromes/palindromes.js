f99c599aed4226443a8300a132d7f124f6b18ba0
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-04-20 13:13:12 +0200

    ex. 9 done (palindromes) first try a mattie

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let lowerCase = input.toLowerCase();
+    let filtered = lowerCase.replace(/[^\w]/g, "");
+    let reversed = "";
+    for (let i = filtered.length - 1; i >=0; i--) {
+        reversed += filtered[i];
+    }
+    
+    if (reversed === filtered) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
