aaa6d7e77f350190a5c7261da7e82e24e3f6b117
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-04-20 13:42:43 +0200

    ex. 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input < 0) {
+        return "OOPS";
+    } else if (+input === 1) {
+        return 1;
+    } else {
+        let prev1 = 0;
+        let prev2 = 1;
+        let current = 0;
+        for (let i = 2; i <= input; i++) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current
+        }
+        return current;
+    }
 };
 
 // Do not edit below this line
