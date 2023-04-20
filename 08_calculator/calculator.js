bed53bb50972a38caa414dcfaa6ec6b75d63a333
Swxkke <68976206+Swxkke@users.noreply.github.com>
2023-04-20 12:51:57 +0200

    ex8 (calculator) done.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,25 +10,43 @@ const subtract = function(a, b) {
 
 const sum = function(sumArray) {
   let equals = 0;
-  for (let number in sumArray) {
-    if (isNaN(number)) {
+  for (let number of sumArray) {
+    if (isNaN(+number)) {
       return equals;
     } else {
-      equals += number
+      equals += (+number);
     }
   }
+  return equals;
 };
 
-const multiply = function(a, b) {
-
+const multiply = function(mulArray) {
+  let equals = 1;
+  for (let number of mulArray) {
+    if (isNaN(+number)) {
+      return equals;
+    } else {
+      equals *= (+number);
+    }
+  }
+  return equals;
 };
 
 const power = function(a, b) {
-	
+	let power = a ** b
+  return power;
 };
 
-const factorial = function(a, b) {
-	
+const factorial = function(number) {
+  let equals = 1;
+  if (number === 0) {
+    return equals;
+  } else {
+    for (let i = number; i >= 1; i--) {
+      equals *= i
+    }
+    return equals;
+  }
 };
 
 // Do not edit below this line
